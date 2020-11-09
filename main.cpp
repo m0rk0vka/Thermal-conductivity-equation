15ccd9bb13f43b70ed01eab744e1db4903643da8
t.vladimir <vovatatosyan2000@mail.ru>
2020-11-09 20:57:06 +0300

    Added realSolv and estimationError function

--- main.cpp
+++ main.cpp
@@ -1,30 +1,102 @@
 #include <iostream>
+#include <cmath>
 
-/* sweepMethod from wiki:
-    a[0] = 0; b[n-1] = 0;
-*/
-void sweepMethod(int N = 1000);
+
+double estimationError (double * u, double * y, int N);
+void realSolv(double * u, int N, double x0);
+void sweepMethod(double * y, int N);
 
 
 int main() {
+    int N;
+    double eps = 0.01, x0 = 0.5;
+
+    std::cout << "Please, enter N:" << std::endl;
+    std::cin >> N;
+
+    while (N < 100000) {
+        double * u;
+        double * y;
+
+        u = new double [N + 1];
+        y = new double [N + 1];
+
+        realSolv(u, N, x0);
+        sweepMethod(y, N);
+
+        for (int i = 0; i < N + 1; i++) {
+            std::cout << "u[i] = " << u[i] << ", y[i] = " << y[i] << std::endl;
+        }
+
+        if (estimationError(u, y, N) < eps) {
+            break;
+        } else {
+            N = 2 * N;
+        }
+
+        delete [] u;
+        delete [] y;
+    }
     return 0;
 }
 
-//error estimate for the solution of a boundary value problem by Runge's rule
-double estimationError(double *y, double *u, int N, int m) {
-    
+
+double estimationError(double * u, double * y, int N) {
+    double res;
+
+    for (int i = 0; i < N + 1; i++) {
+        if (res < abs(u[2 * i] - y[i]) / 3) {
+            res = abs(u[2 * i] - y[i]);
+        }
+    }
+
+    return res;
 }
 
-void sweepMethod(int N = 1000) {
-    double * k, q, fi, x, y;
-    double * alf, bet, a, b, c;
+
+void realSolv(double * u, int N, double x0) {
+    double * x;
+
+    double h = 1 / N;
+    
+    double k = x0 * x0 + 1;
+    double q = x0;
+    double f = exp(-x0); 
+
+    double C = f / (q * ((sqrt(k * q) - 1) * exp(-sqrt(q / k)) - (sqrt(q / k) + 1) * exp(sqrt(q / k))));
+    x = new double [N + 1];
+
+    x[0] = 0.0;
+    for (int i = 1; i < N + 1; i++) {
+        x[i] = i * h;
+    }
+
+    for (int i = 0; i < N + 1; i++) {
+        u[i] = C * (exp(sqrt(q / k) * x[i]) + exp(-sqrt(q / k) * x[i])) + f / q;
+    }
+
+    delete [] x;
+}
+
+
+void sweepMethod(double * y, int N) {
+    double * k;
+    double * q;
+    double * fi;
+    double * x;
+    
+    double * alf;
+    double * bet;
+    double * a;
+    double * b;
+    double * c;
+
     double h = 1.0 / N;
 
     k = new double [N + 1];
     q = new double [N + 1];
     fi = new double [N + 1];
     x = new double [N + 1];
-    y = new double [N + 1];
 
     alf = new double [N + 2];
     bet = new double [N + 2];
@@ -68,7 +140,6 @@ void sweepMethod(int N = 1000) {
     delete [] q;
     delete [] fi;
     delete [] x;
-    delete [] y;
 
     delete [] alf;
     delete [] bet;
